e711a59442d3d83d83660684a7e25b31527deca6
Richard Clemonsuser.email <rguillermoclemons@gmail.com>
2020-07-12 09:45:03 -0400

    Updates

---
+++ precache-manifest.802fb55f37f50617b29f08a06d3e4ee5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a546ba8f150993bbba2aa302e9fff1e4",
+    "url": "/hoopero/index.html"
+  },
+  {
+    "revision": "fb1d71e347449bad276c",
+    "url": "/hoopero/static/css/main.08624717.chunk.css"
+  },
+  {
+    "revision": "c7c29ecb7d911bfb5c4f",
+    "url": "/hoopero/static/js/2.06dafe7b.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/hoopero/static/js/2.06dafe7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb1d71e347449bad276c",
+    "url": "/hoopero/static/js/main.3384d121.chunk.js"
+  },
+  {
+    "revision": "5814e648ff1dd383bba8",
+    "url": "/hoopero/static/js/runtime-main.0c86177d.js"
+  }
+]);
